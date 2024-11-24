39cc24b7afb653b3b0388f2c29252f19ac1ef3d1
= <=>
2024-11-24 13:54:55 -0600

    updated

---
+++ Interac.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Greetings! Please provide your first name?"
+read a
+echo "Welcome! $a, could you please provide your last name?"
+read b
+echo "Thanks $a $b for telling us your name!"
+
+echo "*************************"
+
+echo "Okay $a, it's time to say Good Bye"
---
+++ Math.sh
@@ -0,0 +1,9 @@
+#! /bin/bash/
+
+# division
+
+echo -n 'Enter the first number: '
+read a
+echo -n "Enter the second number: "
+read b
+echo "$a / $b = $((a / b))"
